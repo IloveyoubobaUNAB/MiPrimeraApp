e2a50774b301e60bec5d2a181132235bbb7c069b
Martin Rodriguez <mrodriguez560@unab.edu.co>
2026-03-12 02:01:53 -0500

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mi-ruta-a', function () {
+    return "HOLA ESTA ES MI RUTA";
+});
